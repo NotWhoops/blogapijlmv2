124b883eb967c15913db0f36f61cc8363a23608a
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-27 10:39:59 -0800

    Added Models, Controllers, connection strings

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using blogapijlmv2.Properties.Services;
+using blogapijlmv2.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<BlogItemService>();
+builder.Services.AddScoped<PasswordService>();
+builder.Services.AddScoped<UserService>();
+
+var connectionString = builder.Configuration.GetConnectionString
+("MyBlogString2");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
--- obj/Debug/net9.0/blogapijlmv2.AssemblyInfo.cs
+++ obj/Debug/net9.0/blogapijlmv2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blogapijlmv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1c16ecc1933e4a2075b290fd5f1afbb35640be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a691e5383ff0abebbebe73b7333373fd392f5631")]
 [assembly: System.Reflection.AssemblyProductAttribute("blogapijlmv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blogapijlmv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
